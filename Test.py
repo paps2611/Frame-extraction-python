e7731abbf9693899f25e5e9732f6c92491fb4ff3
Pratyush <pratyushpaps@gmail.com>
2023-07-22 15:04:45 +0530

    added

---
+++ Test.py
@@ -0,0 +1,44 @@
+# Importing all necessary libraries
+import cv2
+import os
+
+
+def main():
+    try:
+
+        # creating a folder named data
+        if not os.path.exists('data'):
+            os.makedirs('data')
+
+    # if not created then raise error
+    except OSError:
+        print('Error: Creating directory of data')
+
+    # frame
+    currentframe = 0
+
+    while (True):
+
+        # reading from frame
+        success, frame = vid.read()
+
+        if success:
+            # continue creating images until video remains
+            name = './data/frame' + str(currentframe) + '.jpeg'
+            print('Creating...' + name)
+
+            # writing the extracted images
+            cv2.imwrite(name, frame)
+
+            # increasing counter so that it will
+            # show how many frames are created
+            currentframe += 1
+        else:
+            break
+
+# Release all space and windows once done
+    vid.release()
+    cv2.destroyAllWindows()
+
+# Read the video from specified path
+vid = cv2.VideoCapture("videos/1/1.mp4")
